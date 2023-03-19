484be8f522381d4c6a7ee4c8dbd6be79a0e9611d
Victor <Victor@VDAVID>
2023-03-19 12:46:08 -0500

    Finish data class

---
+++ ConceptsPOO/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(1985, 11, 31));
+	}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
+
